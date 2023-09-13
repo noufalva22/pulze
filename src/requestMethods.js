dcd30a43239abdb7a5564a03fa5fda954beac588
noufal246 <noufalva@outlook.com>
2023-09-13 19:42:32 +0530

    api link change-1

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -4,6 +4,7 @@ import axios from "axios";
 //to change
 // const BASE_URL = 'http://localhost:5000/api/';
 const BASE_URL = 'https://api.tikktap.com/api/';
+
 const TOKEN= "AAAeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWJkNmYzMWFhYWFhMWY5N2EyMWM3YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NzE2MzY1MSwiZXhwIjoxNjc3MzM2NDUxfQ.KFWBQHnkysTU5AOIv-UN84Jbs5HDg1GYv9hEwVq3NOg"
 
 export const publicRequest = axios.create({
