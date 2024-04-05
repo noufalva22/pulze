e8aad1a69d11c44334c8747375669fb6fe3198dc
noufal246 <noufalva@outlook.com>
2024-04-05 12:14:51 +0530

    backup 05-04-204

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // console.log("URL is", process.env);
 //to change
-const BASE_URL = 'http://localhost:5000/api/';
-// export const BASE_URL = 'https://api.tikktap.com/api/';
+// const BASE_URL = 'http://localhost:5000/api/';
+export const BASE_URL = 'https://api.tikktap.com/api/';
 
 const TOKEN= "AAAeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWJkNmYzMWFhYWFhMWY5N2EyMWM3YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NzE2MzY1MSwiZXhwIjoxNjc3MzM2NDUxfQ.KFWBQHnkysTU5AOIv-UN84Jbs5HDg1GYv9hEwVq3NOg"
 
